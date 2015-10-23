b8ff1b886114ca66c4bcc4f23b1fd71101fe5adb
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2015-10-23 19:43:56 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env::Args;
+use std::env;
+
+fn main() {
+    let addr = parse_cmd_args(env::args());
+    connect_server(addr.0, addr.1)
+}
+
+fn parse_cmd_args(args: Args) -> (String, String) {
+    let args_vec: Vec<_> = args.collect();
+    
+    let host = match args_vec.iter().nth(args_vec.iter().position(|x| x == "h").unwrap()+1) {
+        Some(h) => h,
+        None => panic!("No host defined")
+    };
+
+    let port = match args_vec.iter().nth(args_vec.iter().position(|x| x == "p").unwrap()+1) {
+        Some(p) => p,
+        _ => panic!("No port defined")
+    };
+
+    (host.to_string(), port.to_string())
+}
+
+fn connect_server(host: String, port: String) {
+    println!("Connecting to {}:{}", host, port);
+}
