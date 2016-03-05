1a7498adacdfedd60897c3af92d1a678141c6b4c
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-03-05 19:30:19 +0200

    Refactor args parsing

--- src/connection.rs
+++ src/connection.rs
@@ -1,55 +1,51 @@
 use std::net::{SocketAddr, TcpStream};
 use std::io::{Read, Write};
-use std::str::{FromStr, from_utf8};
 
-pub fn read_message(mut socket: &TcpStream, expected_code: &String) {
-    let cr = 0x0d;
-    let lf = 0x00;
-    let mut trimmed_response = String::new();
+pub fn read_message(mut socket: &TcpStream, expected_code: &str) {
+  let cr = 0x0d;
+  let lf = 0x00;
 
-    loop {
-        let mut line_buffer: Vec<u8> = Vec::new();
-        while line_buffer.len() < 2 || (line_buffer[line_buffer.len()-2] != cr && line_buffer[line_buffer.len()-1] != lf) {
-            let byte_buffer: &mut [u8] = &mut [0];
-            match socket.read(byte_buffer) {
-                Ok(_) => {},
-                Err(_) => panic!("Error reading response"),
-            }
-            line_buffer.push(byte_buffer[0]);
-        }
-        let response = String::from_utf8(line_buffer).unwrap();
-        let chars_to_trim: &[char] = &['\r', '\n'];
-        trimmed_response = response.trim_matches(chars_to_trim).to_string();
-        println!("{}", trimmed_response);
-        let trimmed_response_vec: Vec<char> = trimmed_response.chars().collect();
-        
-        if trimmed_response_vec[3] == ' ' {
-            break;
-        } else {
-            let v: Vec<&str> = trimmed_response.splitn(2, ' ').collect();
-        }
+  loop {
+    let mut line_buffer: Vec<u8> = Vec::new();
+    while line_buffer.len() < 2 || (line_buffer[line_buffer.len()-2] != cr && line_buffer[line_buffer.len()-1] != lf) {
+      let byte_buffer: &mut [u8] = &mut [0];
+      match socket.read(byte_buffer) {
+        Ok(_) => {},
+          Err(_) => panic!("Error reading response"),
+      }
+      line_buffer.push(byte_buffer[0]);
     }
+    let response = String::from_utf8(line_buffer).unwrap();
+    let chars_to_trim: &[char] = &['\r', '\n'];
+    let trimmed_response = response.trim_matches(chars_to_trim).to_string();
+    println!("{}", trimmed_response);
+    let trimmed_response_vec: Vec<char> = trimmed_response.chars().collect();
+
+    if trimmed_response_vec[3] == ' ' {
+      break;
+    }
+  }
 }
 
 pub fn connect(target: SocketAddr) -> TcpStream {
-    let socket = match TcpStream::connect(target) {
-        Ok(s) => s,
-        Err(err) => panic!("Could not bind: {}", err),
-    };
-    socket
+  let socket = match TcpStream::connect(target) {
+    Ok(s) => s,
+      Err(err) => panic!("Could not bind: {}", err),
+  };
+  socket
 }
 
 pub fn send_message(socket: &mut TcpStream, data: Vec<u8>) {
-    match socket.write_all(&data) {
-        Ok(res) => res,
-        Err(err) => panic!("{:?}", err),
-    };
+  match socket.write_all(&data) {
+    Ok(res) => res,
+      Err(err) => panic!("{:?}", err),
+  };
 }
 
 pub fn string_to_addr(host: String, port: String) -> SocketAddr {
-    let addr = match format!("{}:{}",host, port).parse::<SocketAddr>() {
-        Ok(res) => res,
-        Err(err) => panic!("Not a valid address {:?}", err)
-    };
-    addr
+  let addr = match format!("{}:{}",host, port).parse::<SocketAddr>() {
+    Ok(res) => res,
+      Err(err) => panic!("Not a valid address {:?}", err)
+  };
+  addr
 }
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,84 @@
 use std::env::Args;
 use std::env;
-use std::net::{SocketAddr, TcpStream};
+use std::net::{TcpStream};
 use std::io::{self, Write};
 use std::convert::AsRef;
 
 mod connection;
 
 fn main() {
-    let to_addr = parse_cmd_args(env::args());
-    let mut socket = connection::connect(to_addr);
-    connection::read_message(&socket, &"220".to_string());
-    read_username(&mut socket);
-    read_password(&mut socket);
+  let host = parse_cmd_arg(&mut env::args(), "h");
+  let port = parse_cmd_arg(&mut env::args(), "p");
+  let addr = connection::string_to_addr(host, port);
+  let mut socket = connection::connect(addr);
+  connection::read_message(&socket, &"220".to_string());
+  read_username(&mut socket);
+  read_password(&mut socket);
 
-    loop {
-        read_cmd_input(&mut socket);
+  loop {
+    read_cmd_input(&mut socket);
+  }
+}
+
+fn parse_cmd_arg(args: &mut Args, option: &str) -> String {
+  let args_vec: Vec<_> = args.collect();
+
+    match args_vec.iter().nth(args_vec.iter().position(|x| x == option).unwrap()+1) {
+      Some(h) => h.to_string(),
+        None => panic!("No host defined")
     }
 }
 
-fn parse_cmd_args(args: Args) -> SocketAddr {
-    let args_vec: Vec<_> = args.collect();
-    
-    let host = match args_vec.iter().nth(args_vec.iter().position(|x| x == "h").unwrap()+1) {
-        Some(h) => h,
-        None => panic!("No host defined")
-    };
-
-    let port = match args_vec.iter().nth(args_vec.iter().position(|x| x == "p").unwrap()+1) {
-        Some(p) => p,
-        _ => panic!("No port defined")
-    };
-
-    connection::string_to_addr(host.to_string(), port.to_string())
-}
-
 fn read_username(socket: &mut TcpStream) {
-    let mut input = String::new();
-    print!("username: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    let message = "USER ".to_string() + &input;
-    connection::send_message(socket, message.into_bytes().to_vec());
-    connection::read_message(&socket, &"331".to_string());
+  let mut input = String::new();
+  print!("username: ");
+  io::stdout().flush().unwrap();
+  io::stdin().read_line(&mut input).unwrap();
+  let message = "USER ".to_string() + &input;
+  connection::send_message(socket, message.into_bytes().to_vec());
+  connection::read_message(&socket, &"331".to_string());
 }
 
 fn read_password(socket: &mut TcpStream) {
-    let mut input = String::new();
-    print!("password: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    let message = "PASS ".to_string() + &input;
-    connection::send_message(socket, message.into_bytes().to_vec());
-    connection::read_message(&socket, &"230".to_string());
+  let mut input = String::new();
+  print!("password: ");
+  io::stdout().flush().unwrap();
+  io::stdin().read_line(&mut input).unwrap();
+  let message = "PASS ".to_string() + &input;
+  connection::send_message(socket, message.into_bytes().to_vec());
+  connection::read_message(&socket, &"230".to_string());
 }
 
 fn read_cmd_input(socket: &mut TcpStream) {
-    print!("ftp> ");
-    io::stdout().flush().unwrap();
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let args: Vec<&str> = input.split_whitespace().collect();
-    match args[0].as_ref() {
-        "open" => println!("Not implemented"),
-        "cd" => {
-            connection::send_message(socket, format!("CWD {}\r\n", args[1]).to_string().into_bytes().to_vec());
-            connection::read_message(&socket, &"250".to_string());
-        },
-        "pwd" => {
-            connection::send_message(socket, "PWD\r\n".to_string().into_bytes().to_vec());
-            connection::read_message(&socket, &"257".to_string());
-        },
-        "close" => println!("Not implemented"),
-        "active" => println!("Not implemented"),
-        "passive" => println!("Not implemented"),
-        "get" => println!("Not implemented"),
-        "put" => println!("Not implemented"),
-        "ls" => println!("Not implemented"),
-        "ascii" => println!("Not implemented"),
-        "binary" => println!("Not implemented"),
-        "system" => println!("Not implemented"),
-        "status" => println!("Not implemented"),
-        "help" => println!("Not implemented"),
-        "quit" => println!("Not implemented"),
-        _ => println!("unknown command"),
-    };
+  print!("ftp> ");
+  io::stdout().flush().unwrap();
+  let stdin = io::stdin();
+  let mut input = String::new();
+  stdin.read_line(&mut input).unwrap();
+  let args: Vec<&str> = input.split_whitespace().collect();
+  match args[0].as_ref() {
+    "open" => println!("Not implemented"),
+      "cd" => {
+        connection::send_message(socket, format!("CWD {}\r\n", args[1]).to_string().into_bytes().to_vec());
+        connection::read_message(&socket, &"250".to_string());
+      },
+      "pwd" => {
+        connection::send_message(socket, "PWD\r\n".to_string().into_bytes().to_vec());
+        connection::read_message(&socket, &"257".to_string());
+      },
+      "close" => println!("Not implemented"),
+      "active" => println!("Not implemented"),
+      "passive" => println!("Not implemented"),
+      "get" => println!("Not implemented"),
+      "put" => println!("Not implemented"),
+      "ls" => println!("Not implemented"),
+      "ascii" => println!("Not implemented"),
+      "binary" => println!("Not implemented"),
+      "system" => println!("Not implemented"),
+      "status" => println!("Not implemented"),
+      "help" => println!("Not implemented"),
+      "quit" => println!("Not implemented"),
+      _ => println!("unknown command"),
+  };
 }
 
